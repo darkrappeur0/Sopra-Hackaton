2155151553ffdb66bbc0ccdcd0298f1ff316161b
jolisobriquet <dkz.jacques@gmail.com>
2024-10-12 20:47:49 +0200

    Voilà

--- trash/jacques/exempleAffichageFenetre/App.java
+++ trash/jacques/exempleAffichageFenetre/App.java
@@ -17,6 +17,6 @@ public class App extends Application
     public static void main(String[] args)
     {
         launch(args);
-        //System.out.println("Hello world");
+        System.out.println("Hello world");
     }
 }
